a38340f36f61df2e894c73010d4fd0b91d26ff04
Sean Adkinson <sean.adkinson@gmail.com>
2013-06-01 12:56:34 -0700

    Got tweets

--- services/search.js
+++ services/search.js
@@ -1,7 +1,54 @@
 
+var config = require('../config'),
+    request = require('request');
+
+var accessToken;
+
 exports.init = function(app) {
     app.get('/search', function(req, res) {
         console.log("Searching...");
-        res.send("OK");
+        withToken(function() {
+            searchTweets(req, function(tweets) {
+                console.log('Tweets: ', tweets);
+                res.send(tweets);
+            })
+        });
     });
 };
+
+function withToken(cb) {
+    var key = encodeURIComponent(config.twitter.consumerKey) + ':' + encodeURIComponent(config.twitter.consumerSecret);
+    var keyEncoded = new Buffer(key).toString('base64');
+
+    request({
+        url: 'https://api.twitter.com/oauth2/token',
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + keyEncoded
+        },
+        form: {
+            'grant_type': 'client_credentials'
+        }
+    }, function (error, response, body) {
+        accessToken = JSON.parse(body).access_token;
+        console.log("Twitter Access Token: ", accessToken);
+        cb();
+    });
+}
+
+function searchTweets(req, cb) {
+
+    request({
+        url: 'https://api.twitter.com/1.1/statuses/user_timeline.json',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        qs: {
+            'screen_name': req.session.providers[0].username,
+            'count': 200
+        }
+    }, function (error, response, body) {
+        var tweets = JSON.parse(body);
+        cb(tweets);
+    });
+}
